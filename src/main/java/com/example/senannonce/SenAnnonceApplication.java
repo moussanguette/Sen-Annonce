7a3a8c0d2484358466b5829534a98a019d9dc96b
Moussa NGT <mossanguette@gmail.com>
2026-05-11 20:25:12 +0000

    initialisation du projet

---
+++ src/main/java/com/example/senannonce/SenAnnonceApplication.java
@@ -0,0 +1,13 @@
+package com.example.senannonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenAnnonceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenAnnonceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/senannonce/entity/Annonce.java
@@ -0,0 +1,24 @@
+package com.example.senannonce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Annonce {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titre;
+
+    private String description;
+
+    private double prix;
+
+    private String ville;
+
+    private String statut;
+}
