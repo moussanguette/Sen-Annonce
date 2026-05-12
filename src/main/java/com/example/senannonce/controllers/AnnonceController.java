51d4ebbc9047d2caa551aa0222a6626e4a74bf1e
Moussa NGT <mossanguette@gmail.com>
2026-05-12 13:08:53 +0000

    recuperation des annonces OK

--- src/main/java/com/example/senannonce/controllers/AnnonceController.java
+++ src/main/java/com/example/senannonce/controllers/AnnonceController.java
@@ -4,10 +4,9 @@ package com.example.senannonce.controllers;
 import com.example.senannonce.entity.Annonce;
 import com.example.senannonce.services.AnnonceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/annonce")
@@ -25,6 +24,16 @@ public class AnnonceController {
             @RequestParam String statut
     ){
         return annonceService.createAnnonce(titre, description, prix, ville, statut);
+    }
+
+    @GetMapping("/list")
+    public List<Annonce> getAnnonces(){
+        return annonceService.getAllAnnonce();
+    }
+
+    @GetMapping("/{id}")
+    public Annonce getAnnonceById(@PathVariable Long id){
+        return annonceService.getAnnonceById(id);
     }
 
 }
--- src/main/java/com/example/senannonce/services/AnnonceService.java
+++ src/main/java/com/example/senannonce/services/AnnonceService.java
@@ -5,6 +5,9 @@ import com.example.senannonce.repository.AnnonceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AnnonceService {
     @Autowired
@@ -20,5 +23,16 @@ public class AnnonceService {
       annonce.setStatut(statut);
       return annonceRepository.save(annonce);
 
+    }
+
+    // Recuperation de la liste des annonces
+    public List<Annonce> getAllAnnonce(){
+       return annonceRepository.findAll();
+    }
+
+    // Récuperation d'un element de la liste des annonces
+    public Annonce getAnnonceById(Long id){
+        Optional<Annonce> annonce = annonceRepository.findById(id);
+        return annonce.orElse(null);
     }
 }
